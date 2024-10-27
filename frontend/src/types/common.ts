af362be58536a4c6027d9317c8c8ae11fc9120fb
anuragDev <anuragmerndeveloper@gmail.com>
2024-10-27 13:28:01 +0530

    chore: added the husky for better ci cd and fixed the null issue with the icecandiate

--- frontend/src/types/common.ts
+++ frontend/src/types/common.ts
@@ -1,7 +1,7 @@
 interface SocketMessage {
     type: "CREATE_OFFER" | "CREATE_ANSWER" | "ICE_CANDIDATE";
     sdp: RTCSessionDescriptionInit;
-    candidate: RTCIceCandidateInit | null;
+    candidate: RTCIceCandidateInit;
 }
 
 export type { SocketMessage };
